54be993c19d36681c8189d31e98c646b75b50948
Sven Liebig <liebigsv@gmail.com>
2018-09-26 00:55:44 +0200

    update the webpack to clean up the js files after dev mode

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -6,6 +6,8 @@ const {
 const paths = require('./paths');
 const webpackBaseConfig = require('./webpack.basic.config')
 
+const port = 1112
+
 module.exports = webpackMerge(webpackBaseConfig, {
 	mode: "development",
 	entry: {
@@ -28,33 +30,37 @@ module.exports = webpackMerge(webpackBaseConfig, {
 		inline: true,
 		lazy: false,
 		contentBase: path.resolve(__dirname, '..', "dist"),
-		publicPath: 'http://localhost:1112/build',
+		publicPath: `http://localhost:${port}/build`,
 		disableHostCheck: true, // do not use production
 		historyApiFallback: true,
+		clientLogLevel: 'info',
+		quiet: true,
 		// hot: true,
 		port: 1112,
 		before() {
-			spawn(
-					'yarn', ['run-electron'], {
+			spawn('yarn', ['run-electron'], {
+					shell: true,
+					env: process.env,
+					stdio: 'inherit'
+				})
+				.on('close', code => {
+					console.log(`The Electron Process was closed.`)
+					spawn("yarn", ["clean:electron"], {
 						shell: true,
 						env: process.env,
 						stdio: 'inherit'
-					}
-				)
-				.on('close', code => process.exit(code))
+					}).on('close', (code) => {
+						console.log(`Cleaned up the temporary JavaScript files.`)
+						process.exit(code)
+					})
+				})
 				.on('error', spawnError => console.error(spawnError));
 		},
 		after() {
-			// setTimeout(() => {
-			// 	spawn("yarn", ["clean:electron"], {
-			// 		shell: true,
-			// 		env: process.env,
-			// 		stdio: 'inherit'
-			// 	}).on('close', (code) => {
-			// 		console.log("Remove JS Files.")
-			// 		// process.exit(code)
-			// 	})
-			// }, 2000)
+			console.log(`webpack-dev-server is running at http://localhost:${port}`)
+			console.log(`you can debug the applications launcher in your own browser:`)
+			console.log(`- http://localhost:${port}/build/launcher.html`)
+			console.log(`- http://localhost:${port}/build/settings.html`)
 		},
 
 	},
